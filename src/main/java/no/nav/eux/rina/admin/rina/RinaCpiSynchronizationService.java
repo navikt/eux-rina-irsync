c40f641f274175f5403f79e16c7d0626bd07b922
Kirschner, Torsten <Torsten.Kirschner@nav.no>
2021-04-13 08:51:43 +0200

    6.2.5-SNAPSHOT supporting both RINA 2019 and RINA 2020

--- src/main/java/no/nav/eux/rina/admin/rina/RinaCpiSynchronizationService.java
+++ src/main/java/no/nav/eux/rina/admin/rina/RinaCpiSynchronizationService.java
@@ -44,32 +44,22 @@ public class RinaCpiSynchronizationService {
   @SneakyThrows
   public SyncInitialDocumentDto getInititalDocument() {
     SyncInitialDocumentDto syncInitialDocumentDto = syncApi.retrieveInitialDocumentIR();
-//    log.info("gid0 [{} : {}]", rinaTenant.getInstitutionId(), ToStringBuilder.reflectionToString(syncInitialDocumentDto));
     List<ResourceDto> resourceDtoList = resourcesApi.getResources("SERVER", true, Collections.singletonList("organisation"));
     resourceDtoList.sort(new Comparator<ResourceDto>() {
       @Override
       public int compare(ResourceDto o1, ResourceDto o2) {
         Semver v1 = new Semver(o1.getVersion());
-        //log.info("v1 = [{}]", v1);
         Semver v2 = new Semver(o2.getVersion());
-        //log.info("v2 = [{}]", v2);
-        //log.info("v1.compareTo(v2) = [{}]", v1.compareTo(v2));
         return v1.compareTo(v2);
       }
     });
     String currentVersion = resourceDtoList.get(resourceDtoList.size() - 1).getVersion();
-//    log.info("currentVersion from resourcesApi [{}]", currentVersion);
     Semver semver = new Semver(currentVersion);
-//    log.info("semver from currentVersion [{}] and withClearedSuffixAndBuild [{}]", semver, semver.withClearedSuffixAndBuild());
     PropertyUtils.setProperty(syncInitialDocumentDto, "initialDocument.SYN002.RequestForIRSync.currentVersion", semver.withClearedSuffixAndBuild().toString());
-//    log.info("upd1 [{}]", ToStringBuilder.reflectionToString(syncInitialDocumentDto));
-    return syncInitialDocumentDto; //.putInitialDocumentItem("initalDocument", resourceDtoList.get(resourceDtoList.size()-1));
+    return syncInitialDocumentDto;
   }
   
   public void submitDocumentIR(SyncInitialDocumentDto dto, String defaultVersion) {
-//    log.info("submitting a syn002 [{}] with defaultVersion {}", dto.getInitialDocument(), defaultVersion);
-//    log.info("sub0 [{}]", ToStringBuilder.reflectionToString(dto));
-    
     try {
       PropertyUtils.setProperty(dto, "initialDocument.SYN002.RequestForIRSync.currentVersion", defaultVersion);
     } catch (IllegalAccessException e) {
@@ -79,11 +69,7 @@ public class RinaCpiSynchronizationService {
     } catch (NoSuchMethodException e) {
       log.error("NoSuchMethodException " + e.getMessage());
     }
-//    log.info("SIRn [{}]", ToStringBuilder.reflectionToString(defaultVersion));
-    
     try {
-//      log.info("subz dto [{}]", ToStringBuilder.reflectionToString(dto));
-//      log.info("subz inD [{}]", ToStringBuilder.reflectionToString(dto.getInitialDocument()));
       syncApi.submitDocumentIR(dto.getInitialDocument());
     } catch (RestClientException e) {
       log.error("Could not submit IR document!" + e);
@@ -92,34 +78,25 @@ public class RinaCpiSynchronizationService {
   
   public Map<String, String> getInstitutionVersions() {
     String ci = rinaTenant.getInstitutionId();
-//    log.debug("rinaTenant.institutionId = [" + ci + "]");
     Map<String, String> resultCiVersions = new HashMap<>();
     Semver syn002Version = new Semver(getCurrentIrVersionFromSYN002(getInititalDocument()), Semver.SemverType.STRICT);
-//    log.info("syn002version = [{}]", syn002Version);
-    // syn002Version seems ALWAYS to be identical availableVersion.withClearedSuffixAndBuild()
     resultCiVersions.put("SYN002", syn002Version.toString());
     
     ResourceDto installedDto = getResources("SERVER", true, Collections.singletonList("organisation")).stream()
       .reduce((first, second) -> second)
       .get(); // List.last()
     Semver installedVersion = new Semver(installedDto.getVersion(), Semver.SemverType.STRICT);
-//    log.info("installedVersion = [{}]", installedVersion);
     resultCiVersions.put("INSTALLED", installedVersion.toString());
     
     ResourceDto availableDto = getResources("DISK", true, Collections.singletonList("organisation")).stream()
       .reduce((first, second) -> second)
       .get(); // List.last()
     Semver availableVersion = new Semver(availableDto.getVersion(), Semver.SemverType.STRICT);
-//    log.info("availableVersion = [{}]", availableVersion);
     resultCiVersions.put("AVAILABLE", availableVersion.toString());
     
     if (installedVersion.withClearedSuffixAndBuild().isLowerThan(availableVersion.withClearedSuffixAndBuild())) {
       log.info("CI: " + ci + " OUT OF DATE : availableVersion " + availableVersion + " is > installed " + installedVersion);
       resultCiVersions.put("ACTION", "UPDATE");
-      //  log.info("CI: " + ci + " Installing now... resource version: " + availableVersion);
-      //  getRinaCpiSynchronizationsService(ci)
-      //    .updateResource(resourceId, resourceType, availableVersion.toString());
-      //  log.info("CI: " + ci + " Installed resourceId[" + resourceId + "] resourceType [" + resourceType + "] resourceVersion [" + availableVersion + "]");
     } else {
       log.info(" UP TO DATE : " + ci + " installedVersion " + installedVersion + " is >= available " + availableVersion);
       resultCiVersions.put("ACTION", "NONE");
@@ -148,7 +125,6 @@ public class RinaCpiSynchronizationService {
   
   private String getCurrentIrVersionFromSYN002(SyncInitialDocumentDto dto) {
     String currentVersion = "";
-//    log.info("get0 [{}]", ToStringBuilder.reflectionToString(dto));
     try {
       currentVersion = (String) PropertyUtils.getProperty(dto, "initialDocument.SYN002.RequestForIRSync.currentVersion");
     } catch (IllegalAccessException e) {
